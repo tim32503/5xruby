06044c3fdcdd762e9e5ddc5a8846b715ed6ae38d
Tim Hsueh <timhsueh@curihaosity.xyz>
2021-05-08 23:36:56 +0800

    Update: 0506-糖果題

---
+++ 0506/test.js
@@ -0,0 +1,13 @@
+// 糖果題：爸爸的年紀在幾年之後就會是兒子的 2 倍？
+// 難度：一顆糖
+// 期限：5/6 中午 12:00 前
+
+function DoubleAge(dad, son) {
+  // 實作內容
+  return dad - 2 * son
+}
+
+console.log(DoubleAge(36, 7))   // 印出 22
+console.log(DoubleAge(42, 21))  // 印出 0
+console.log(DoubleAge(22, 1))   // 印出 20
+console.log(DoubleAge(29, 0))   // 印出 29
